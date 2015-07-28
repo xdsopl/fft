2014dc46fcef32fe48d58f0bc00e24a4dba4aeb4
Ahmet Inan <xdsopl@googlemail.com>
2015-07-28 21:22:54 +0200

    removed redundant code using template parameter SIGN
    also using twiddle functions instead of static constants.
    interestingly this time the compiler is able to inline the constant.

--- fft.hh
+++ fft.hh
@@ -11,6 +11,19 @@ You should have received a copy of the CC0 Public Domain Dedication along with t
 namespace FFT {
 
 template <typename TYPE>
+static inline TYPE half(TYPE a) { return typename TYPE::value_type(0.5) * a; }
+template <typename TYPE>
+static inline TYPE sqrt3(TYPE a) { return std::sqrt(typename TYPE::value_type(3)) * a; }
+template <typename TYPE>
+static inline TYPE cos2pi5(TYPE a) { return std::cos(typename TYPE::value_type(2 * M_PI / 5)) * a; }
+template <typename TYPE>
+static inline TYPE sin2pi5(TYPE a) { return std::sin(typename TYPE::value_type(2 * M_PI / 5)) * a; }
+template <typename TYPE>
+static inline TYPE cos4pi5(TYPE a) { return std::cos(typename TYPE::value_type(4 * M_PI / 5)) * a; }
+template <typename TYPE>
+static inline TYPE sin4pi5(TYPE a) { return std::sin(typename TYPE::value_type(4 * M_PI / 5)) * a; }
+
+template <typename TYPE>
 static inline TYPE twiddle(TYPE a, TYPE b)
 {
 	return TYPE(a.imag() - b.imag(), b.real() - a.real());
@@ -31,82 +44,68 @@ static constexpr int split(int N)
 	return (!(N%5)) ? 5 : (!(N%3)) ? 3 : (!(N%2)&&pow4(N)) ? 4 : (!(N%2)) ? 2 : 1;
 }
 
-template <int RADIX, int N, int S, typename TYPE>
+template <int RADIX, int N, int S, typename TYPE, int SIGN>
 struct Dit {};
 
-template <int S, typename TYPE>
-struct Dit<1, 1, S, TYPE>
+template <int S, typename TYPE, int SIGN>
+struct Dit<1, 1, S, TYPE, SIGN>
 {
-	static inline void fwd(TYPE *out, TYPE *in, TYPE *)
-	{
-		*out = *in;
-	}
-	static inline void bwd(TYPE *out, TYPE *in, TYPE *)
+	static inline void dit(TYPE *out, TYPE *in, TYPE *)
 	{
 		*out = *in;
 	}
 };
 
-template <int S, typename TYPE>
-struct Dit<2, 2, S, TYPE>
+template <int S, typename TYPE, int SIGN>
+struct Dit<2, 2, S, TYPE, SIGN>
 {
-	static inline void fwd(TYPE *out0, TYPE *out1, TYPE in0, TYPE in1)
+	static inline void dft(TYPE *out0, TYPE *out1, TYPE in0, TYPE in1)
 	{
 		*out0 = in0 + in1;
 		*out1 = in0 - in1;
 	}
-	static inline void bwd(TYPE *out0, TYPE *out1, TYPE in0, TYPE in1)
+	static inline void dit(TYPE *out, TYPE *in, TYPE *)
 	{
-		*out0 = in0 + in1;
-		*out1 = in0 - in1;
-	}
-	static inline void fwd(TYPE *out, TYPE *in, TYPE *)
-	{
-		fwd(out, out + 1, in[0], in[S]);
-	}
-	static inline void bwd(TYPE *out, TYPE *in, TYPE *)
-	{
-		bwd(out, out + 1, in[0], in[S]);
+		dft(out, out + 1, in[0], in[S]);
 	}
 };
 
 template <int S, typename TYPE>
-struct Dit<3, 3, S, TYPE>
+struct Dit<3, 3, S, TYPE, -1>
 {
-	typedef typename TYPE::value_type value_type;
-	static constexpr value_type half = value_type(1) / value_type(2);
-#if 0
-	static constexpr value_type sqrt3 = std::sqrt(value_type(3));
-#else
-	static constexpr value_type sqrt3 = 1.732050807568877;
-#endif
-	static inline void fwd(TYPE *out0, TYPE *out1, TYPE *out2, TYPE in0, TYPE in1, TYPE in2)
+	static inline void dft(TYPE *out0, TYPE *out1, TYPE *out2, TYPE in0, TYPE in1, TYPE in2)
 	{
-		TYPE a(in1 + in2), b(sqrt3 * twiddle(in2, in1));
+		TYPE a(in1 + in2), b(sqrt3(twiddle(in2, in1)));
 		*out0 = in0 + a;
-		*out1 = in0 - half * (a + b);
-		*out2 = in0 - half * (a - b);
+		*out1 = in0 - half(a + b);
+		*out2 = in0 - half(a - b);
 	}
-	static inline void bwd(TYPE *out0, TYPE *out1, TYPE *out2, TYPE in0, TYPE in1, TYPE in2)
+	static inline void dit(TYPE *out, TYPE *in, TYPE *)
 	{
-		TYPE a(in1 + in2), b(sqrt3 * twiddle(in2, in1));
-		*out0 = in0 + a;
-		*out1 = in0 - half * (a - b);
-		*out2 = in0 - half * (a + b);
-	}
-	static inline void fwd(TYPE *out, TYPE *in, TYPE *)
-	{
-		fwd(out, out + 1, out + 2, in[0], in[S], in[2 * S]);
-	}
-	static inline void bwd(TYPE *out, TYPE *in, TYPE *) {
-		bwd(out, out + 1, out + 2, in[0], in[S], in[2 * S]);
+		dft(out, out + 1, out + 2, in[0], in[S], in[2 * S]);
 	}
 };
 
 template <int S, typename TYPE>
-struct Dit<4, 4, S, TYPE>
+struct Dit<3, 3, S, TYPE, 1>
 {
-	static inline void fwd(TYPE *out0, TYPE *out1, TYPE *out2, TYPE *out3,
+	static inline void dft(TYPE *out0, TYPE *out1, TYPE *out2, TYPE in0, TYPE in1, TYPE in2)
+	{
+		TYPE a(in1 + in2), b(sqrt3(twiddle(in2, in1)));
+		*out0 = in0 + a;
+		*out1 = in0 - half(a - b);
+		*out2 = in0 - half(a + b);
+	}
+	static inline void dit(TYPE *out, TYPE *in, TYPE *)
+	{
+		dft(out, out + 1, out + 2, in[0], in[S], in[2 * S]);
+	}
+};
+
+template <int S, typename TYPE>
+struct Dit<4, 4, S, TYPE, -1>
+{
+	static inline void dft(TYPE *out0, TYPE *out1, TYPE *out2, TYPE *out3,
 			TYPE in0, TYPE in1, TYPE in2, TYPE in3)
 	{
 		TYPE a(in0 + in2), b(in0 - in2), c(in1 + in3), d(twiddle(in1, in3));
@@ -115,7 +114,16 @@ struct Dit<4, 4, S, TYPE>
 		*out2 = a - c;
 		*out3 = b - d;
 	}
-	static inline void bwd(TYPE *out0, TYPE *out1, TYPE *out2, TYPE *out3,
+	static inline void dit(TYPE *out, TYPE *in, TYPE *)
+	{
+		dft(out, out + 1, out + 2, out + 3, in[0], in[S], in[2 * S], in[3 * S]);
+	}
+};
+
+template <int S, typename TYPE>
+struct Dit<4, 4, S, TYPE, 1>
+{
+	static inline void dft(TYPE *out0, TYPE *out1, TYPE *out2, TYPE *out3,
 			TYPE in0, TYPE in1, TYPE in2, TYPE in3)
 	{
 		TYPE a(in0 + in2), b(in0 - in2), c(in1 + in3), d(twiddle(in1, in3));
@@ -124,151 +132,104 @@ struct Dit<4, 4, S, TYPE>
 		*out2 = a - c;
 		*out3 = b + d;
 	}
-	static inline void fwd(TYPE *out, TYPE *in, TYPE *)
+	static inline void dit(TYPE *out, TYPE *in, TYPE *)
 	{
-		fwd(out, out + 1, out + 2, out + 3, in[0], in[S], in[2 * S], in[3 * S]);
-	}
-	static inline void bwd(TYPE *out, TYPE *in, TYPE *)
-	{
-		bwd(out, out + 1, out + 2, out + 3, in[0], in[S], in[2 * S], in[3 * S]);
+		dft(out, out + 1, out + 2, out + 3, in[0], in[S], in[2 * S], in[3 * S]);
 	}
 };
 
 template <int S, typename TYPE>
-struct Dit<5, 5, S, TYPE>
+struct Dit<5, 5, S, TYPE, -1>
 {
-	typedef typename TYPE::value_type value_type;
-#if 0
-	static constexpr value_type r1 = 2 * M_PI / 5;
-	static constexpr value_type c1 = std::cos(r1);
-	static constexpr value_type s1 = std::sin(r1);
-	static constexpr value_type r2 = 4 * M_PI / 5;
-	static constexpr value_type c2 = std::cos(r2);
-	static constexpr value_type s2 = std::sin(r2);
-#else
-	static constexpr value_type c1 = 0.3090169943749475;
-	static constexpr value_type s1 = 0.9510565162951535;
-	static constexpr value_type c2 = -0.8090169943749473;
-	static constexpr value_type s2 = 0.5877852522924732;
-#endif
-	static inline void fwd(TYPE *out0, TYPE *out1, TYPE *out2, TYPE *out3, TYPE *out4,
+	static inline void dft(TYPE *out0, TYPE *out1, TYPE *out2, TYPE *out3, TYPE *out4,
 			TYPE in0, TYPE in1, TYPE in2, TYPE in3, TYPE in4)
 	{
 		TYPE a(in1 + in4), b(in2 + in3), c(twiddle(in1, in4)), d(twiddle(in2, in3));
-		TYPE c1a(c1 * a), c2b(c2 * b), s1c(s1 * c), s2d(s2 * d);
-		TYPE c2a(c2 * a), c1b(c1 * b), s2c(s2 * c), s1d(s1 * d);
+		TYPE c1a(cos2pi5(a)), c2b(cos4pi5(b)), s1c(sin2pi5(c)), s2d(sin4pi5(d));
+		TYPE c2a(cos4pi5(a)), c1b(cos2pi5(b)), s2c(sin4pi5(c)), s1d(sin2pi5(d));
 		*out0 = in0 + a + b;
 		*out1 = in0 + c1a + c2b + s1c + s2d;
 		*out2 = in0 + c2a + c1b + s2c - s1d;
 		*out3 = in0 + c2a + c1b - s2c + s1d;
 		*out4 = in0 + c1a + c2b - s1c - s2d;
 	}
-	static inline void bwd(TYPE *out0, TYPE *out1, TYPE *out2, TYPE *out3, TYPE *out4,
+	static inline void dit(TYPE *out, TYPE *in, TYPE *)
+	{
+		dft(out, out + 1, out + 2, out + 3, out + 4, in[0], in[S], in[2 * S], in[3 * S], in[4 * S]);
+	}
+};
+
+template <int S, typename TYPE>
+struct Dit<5, 5, S, TYPE, 1>
+{
+	static inline void dft(TYPE *out0, TYPE *out1, TYPE *out2, TYPE *out3, TYPE *out4,
 			TYPE in0, TYPE in1, TYPE in2, TYPE in3, TYPE in4)
 	{
 		TYPE a(in1 + in4), b(in2 + in3), c(twiddle(in1, in4)), d(twiddle(in2, in3));
-		TYPE c1a(c1 * a), c2b(c2 * b), s1c(s1 * c), s2d(s2 * d);
-		TYPE c2a(c2 * a), c1b(c1 * b), s2c(s2 * c), s1d(s1 * d);
+		TYPE c1a(cos2pi5(a)), c2b(cos4pi5(b)), s1c(sin2pi5(c)), s2d(sin4pi5(d));
+		TYPE c2a(cos4pi5(a)), c1b(cos2pi5(b)), s2c(sin4pi5(c)), s1d(sin2pi5(d));
 		*out0 = in0 + a + b;
 		*out1 = in0 + c1a + c2b - s1c - s2d;
 		*out2 = in0 + c2a + c1b - s2c + s1d;
 		*out3 = in0 + c2a + c1b + s2c - s1d;
 		*out4 = in0 + c1a + c2b + s1c + s2d;
 	}
-	static inline void fwd(TYPE *out, TYPE *in, TYPE *)
+	static inline void dit(TYPE *out, TYPE *in, TYPE *)
 	{
-		fwd(out, out + 1, out + 2, out + 3, out + 4, in[0], in[S], in[2 * S], in[3 * S], in[4 * S]);
-	}
-	static inline void bwd(TYPE *out, TYPE *in, TYPE *) {
-		bwd(out, out + 1, out + 2, out + 3, out + 4, in[0], in[S], in[2 * S], in[3 * S], in[4 * S]);
+		dft(out, out + 1, out + 2, out + 3, out + 4, in[0], in[S], in[2 * S], in[3 * S], in[4 * S]);
 	}
 };
 
-template <int N, int S, typename TYPE>
-struct Dit<2, N, S, TYPE>
+template <int N, int S, typename TYPE, int SIGN>
+struct Dit<2, N, S, TYPE, SIGN>
 {
-	static void fwd(TYPE *out, TYPE *in, TYPE *z)
+	static void dit(TYPE *out, TYPE *in, TYPE *z)
 	{
 		for (int o = 0, i = 0; o < N; o += N / 2, i += S)
-			Dit<split(N / 2), N / 2, 2 * S, TYPE>::fwd(out + o, in + i, z);
+			Dit<split(N / 2), N / 2, 2 * S, TYPE, SIGN>::ditd(out + o, in + i, z);
 		for (int k0 = 0, k1 = N / 2; k0 < N / 2; ++k0, ++k1)
-			Dit<2, 2, S, TYPE>::fwd(out + k0, out + k1, out[k0], z[k0 * S] * out[k1]);
-	}
-	static void bwd(TYPE *out, TYPE *in, TYPE *z)
-	{
-		for (int o = 0, i = 0; o < N; o += N / 2, i += S)
-			Dit<split(N / 2), N / 2, 2 * S, TYPE>::bwd(out + o, in + i, z);
-		for (int k0 = 0, k1 = N / 2; k0 < N / 2; ++k0, ++k1)
-			Dit<2, 2, S, TYPE>::bwd(out + k0, out + k1, out[k0], z[k0 * S] * out[k1]);
+			Dit<2, 2, S, TYPE, SIGN>::dft(out + k0, out + k1, out[k0], z[k0 * S] * out[k1]);
 	}
 };
 
-template <int N, int S, typename TYPE>
-struct Dit<3, N, S, TYPE>
+template <int N, int S, typename TYPE, int SIGN>
+struct Dit<3, N, S, TYPE, SIGN>
 {
-	static void fwd(TYPE *out, TYPE *in, TYPE *z)
+	static void dit(TYPE *out, TYPE *in, TYPE *z)
 	{
 		for (int o = 0, i = 0; o < N; o += N / 3, i += S)
-			Dit<split(N / 3), N / 3, 3 * S, TYPE>::fwd(out + o, in + i, z);
+			Dit<split(N / 3), N / 3, 3 * S, TYPE, SIGN>::dit(out + o, in + i, z);
 		for (int k0 = 0, k1 = N / 3, k2 = 2 * N / 3; k0 < N / 3; ++k0, ++k1, ++k2)
-			Dit<3, 3, S, TYPE>::fwd(out + k0, out + k1, out + k2,
-				out[k0], z[k0 * S] * out[k1], z[2 * k0 * S] * out[k2]);
-	}
-	static void bwd(TYPE *out, TYPE *in, TYPE *z)
-	{
-		for (int o = 0, i = 0; o < N; o += N / 3, i += S)
-			Dit<split(N / 3), N / 3, 3 * S, TYPE>::bwd(out + o, in + i, z);
-		for (int k0 = 0, k1 = N / 3, k2 = 2 * N / 3; k0 < N / 3; ++k0, ++k1, ++k2)
-			Dit<3, 3, S, TYPE>::bwd(out + k0, out + k1, out + k2,
+			Dit<3, 3, S, TYPE, SIGN>::dft(out + k0, out + k1, out + k2,
 				out[k0], z[k0 * S] * out[k1], z[2 * k0 * S] * out[k2]);
 	}
 };
 
-template <int N, int S, typename TYPE>
-struct Dit<4, N, S, TYPE>
+template <int N, int S, typename TYPE, int SIGN>
+struct Dit<4, N, S, TYPE, SIGN>
 {
-	static void fwd(TYPE *out, TYPE *in, TYPE *z)
+	static void dit(TYPE *out, TYPE *in, TYPE *z)
 	{
 		for (int o = 0, i = 0; o < N; o += N / 4, i += S)
-			Dit<split(N / 4), N / 4, 4 * S, TYPE>::fwd(out + o, in + i, z);
+			Dit<split(N / 4), N / 4, 4 * S, TYPE, SING>::dit(out + o, in + i, z);
 		for (int k0 = 0, k1 = N / 4, k2 = 2 * N / 4, k3 = 3 * N / 4;
 				k0 < N / 4; ++k0, ++k1, ++k2, ++k3)
-			Dit<4, 4, S, TYPE>::fwd(out + k0, out + k1, out + k2, out + k3,
-				out[k0], z[k0 * S] * out[k1], z[2 * k0 * S] * out[k2],
-				z[3 * k0 * S] * out[k3]);
-	}
-	static void bwd(TYPE *out, TYPE *in, TYPE *z)
-	{
-		for (int o = 0, i = 0; o < N; o += N / 4, i += S)
-			Dit<split(N / 4), N / 4, 4 * S, TYPE>::bwd(out + o, in + i, z);
-		for (int k0 = 0, k1 = N / 4, k2 = 2 * N / 4, k3 = 3 * N / 4;
-				k0 < N / 4; ++k0, ++k1, ++k2, ++k3)
-			Dit<4, 4, S, TYPE>::bwd(out + k0, out + k1, out + k2, out + k3,
+			Dit<4, 4, S, TYPE, SIGN>::dft(out + k0, out + k1, out + k2, out + k3,
 				out[k0], z[k0 * S] * out[k1], z[2 * k0 * S] * out[k2],
 				z[3 * k0 * S] * out[k3]);
 	}
 };
 
-template <int N, int S, typename TYPE>
-struct Dit<5, N, S, TYPE>
+template <int N, int S, typename TYPE, int SIGN>
+struct Dit<5, N, S, TYPE, SIGN>
 {
-	static void fwd(TYPE *out, TYPE *in, TYPE *z)
+	static void dit(TYPE *out, TYPE *in, TYPE *z)
 	{
 		for (int o = 0, i = 0; o < N; o += N / 5, i += S)
-			Dit<split(N / 5), N / 5, 5 * S, TYPE>::fwd(out + o, in + i, z);
+			Dit<split(N / 5), N / 5, 5 * S, TYPE, SIGN>::dit(out + o, in + i, z);
 		for (int k0 = 0, k1 = N / 5, k2 = 2 * N / 5, k3 = 3 * N / 5, k4 = 4 * N / 5;
 				k0 < N / 5; ++k0, ++k1, ++k2, ++k3, ++k4)
-			Dit<5, 5, S, TYPE>::fwd(out + k0, out + k1, out + k2, out + k3, out + k4,
-				out[k0], z[k0 * S] * out[k1], z[2 * k0 * S] * out[k2],
-				z[3 * k0 * S] * out[k3], z[4 * k0 * S] * out[k4]);
-	}
-	static void bwd(TYPE *out, TYPE *in, TYPE *z)
-	{
-		for (int o = 0, i = 0; o < N; o += N / 5, i += S)
-			Dit<split(N / 5), N / 5, 5 * S, TYPE>::bwd(out + o, in + i, z);
-		for (int k0 = 0, k1 = N / 5, k2 = 2 * N / 5, k3 = 3 * N / 5, k4 = 4 * N / 5;
-				k0 < N / 5; ++k0, ++k1, ++k2, ++k3, ++k4)
-			Dit<5, 5, S, TYPE>::bwd(out + k0, out + k1, out + k2, out + k3, out + k4,
+			Dit<5, 5, S, TYPE, SIGN>::dft(out + k0, out + k1, out + k2, out + k3, out + k4,
 				out[k0], z[k0 * S] * out[k1], z[2 * k0 * S] * out[k2],
 				z[3 * k0 * S] * out[k3], z[4 * k0 * S] * out[k4]);
 	}
@@ -294,7 +255,7 @@ public:
 	typedef typename TYPE::value_type value_type;
 	inline void operator ()(TYPE *out, TYPE *in)
 	{
-		Dit<split(BINS), BINS, 1, TYPE>::fwd(out, in, factors.z);
+		Dit<split(BINS), BINS, 1, TYPE, -1>::dit(out, in, factors.z);
 	}
 };
 
@@ -306,7 +267,7 @@ public:
 	typedef typename TYPE::value_type value_type;
 	inline void operator ()(TYPE *out, TYPE *in)
 	{
-		Dit<split(BINS), BINS, 1, TYPE>::bwd(out, in, factors.z);
+		Dit<split(BINS), BINS, 1, TYPE, 1>::dit(out, in, factors.z);
 	}
 };
 
